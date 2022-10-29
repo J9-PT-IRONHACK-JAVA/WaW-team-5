c6324fcbcd75ff356d8a12491c579a229ffe5554
Elisabet Aguilar <111438156+elisabetaguilar@users.noreply.github.com>
2022-10-29 15:38:37 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+package com.ironhack.wawgame;
+
+import com.ironhack.wawgame.gameServices.Game;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        while (true) {
+            String resp = game.askSomethingToUser("Do you want to play a game?");
+            if (resp.equals("yes")) {
+                // crear lista de characters 1;
+                // crear lista de characters 2;
+//
+                // team1 = new team(lista de characters1);
+                // team2 = new team(lista de characters2);
+//
+                // battle = new batle(team1, team2);
+//
+                // battle.initilizeGame();
+//
+                // while (!battle.isFinished()) {
+
+                //     battle.NextRound();
+                // }
+
+
+            } else {
+                break;
+            }
+        }
+    }
+}
