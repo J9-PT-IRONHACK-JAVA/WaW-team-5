3992e0646b1dfb7e9e3515702d209212f477b590
jose <jose245198@gmail.com>
2022-11-05 16:21:22 +0100

    new commit

--- src/com/ironhack/wawgame/gameObjects/Party.java
+++ src/com/ironhack/wawgame/gameObjects/Party.java
@@ -7,17 +7,28 @@ public class Party {
     private int size;
     private final int MAX_SIZE_PARTY = 5;
 
-    public Character chooseCharacter(String name) {
-        String string = " "
-        // decide if choose character by id or by name
-        // add logic for select character by name
-        // check if exist
-        return new Character(); // return character if found else return null
+    public void addCharacter(Character character) {
+        if (size < MAX_SIZE_PARTY) {
+            party.add(character);
+            size++;
+        } else {
+            System.out.println("The party is full");
+        }
     }
-    AddCharacter
+
+
+
+    addCharacter
     chooseCharacter();
     RemoveCharacter
     // Add addCharacter method, check if the party if full, increase size attribute by 1, add character to party attribute
     // Add removeCharacter method (by name?, by ID) , check if character exist
     // add size method that return size attribute
 }
+    //public Character chooseCharacter(String name) {
+    String string = " "
+// decide if choose character by id or by name
+// add logic for select character by name
+// check if exist
+        return new Character(); // return character if found else return null
+                }
