d12e2158eeaa352756cfb6fedcbe173c3b2613d2
Pablo Martinez <115550667+PabloMartinez00@users.noreply.github.com>
2022-11-06 17:55:41 +0100

    BattleMenu updated Main: line 22 corrected Battle: removed souts

--- src/main/java/com/ironhack/wawgame/gameMenus/BattleMenu.java
+++ src/main/java/com/ironhack/wawgame/gameMenus/BattleMenu.java
@@ -1,4 +1,48 @@
 package com.ironhack.wawgame.gameMenus;
 
+import com.ironhack.wawgame.gameObjects.Character;
+import com.ironhack.wawgame.gameObjects.Warrior;
+import com.ironhack.wawgame.gameObjects.Wizard;
+
+import java.util.ArrayList;
+
 public class BattleMenu {
+
+    //Comienzo de la batalla
+    public  static void battleBegins () {
+        System.out.println("▒█▀▀█ █▀▀█ ▀▀█▀▀ ▀▀█▀▀ █░░ █▀▀ 　 █▀▀▄ █▀▀ █▀▀▀ ░▀░ █▀▀▄ █▀▀");
+        System.out.println("▒█▀▀▄ █▄▄█ ░░█░░ ░░█░░ █░░ █▀▀ 　 █▀▀▄ █▀▀ █░▀█ ▀█▀ █░░█ ▀▀█");
+        System.out.println("▒█▄▄█ ▀░░▀ ░░▀░░ ░░▀░░ ▀▀▀ ▀▀▀ 　 ▀▀▀░ ▀▀▀ ▀▀▀▀ ▀▀▀ ▀░░▀ ▀▀▀");
+        System.out.println("⚔️️️️️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️");
+    }
+
+    //Tras acabar la batalla, preguntar si desea otra batalla
+    public static void askIfStartNewBattle () {
+        System.out.println("Do you want to start a new battle (Y/N)?");
+    }
+
+
+    //Confirmación que las partys se han generado
+    public static void partysGenerated () {
+        System.out.println("Parties generated. Battle ready to start");
+    }
+
+    //Imprimir los componentes de cada party para Battle
+    public static void printPartiesCharacters (int partyNumber, ArrayList<Character> party) {
+        System.out.println("PARTY " + partyNumber + " combatants");
+        System.out.println("********************************");
+        System.out.println("\uD83E\uDDD9\u200D️ ᗯｴƵ\uD835\uDCD0ᏒƊS: ");
+        for (Character character : party) {
+            if (character instanceof Wizard) {
+                System.out.println(character);
+            }
+        }
+        System.out.println("⚔️ ᗯ\uD835\uDCD0ᎡᏒｴOᏒS: ");
+        for (Character character : party) {
+            if (character instanceof Warrior) {
+                System.out.println(character);
+            }
+        }
+    }
+
 }
