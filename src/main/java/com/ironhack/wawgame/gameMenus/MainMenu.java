55461d3fa5f1a1c5b363455d56a94a937072ee7c
jose <jose245198@gmail.com>
2022-11-08 21:15:17 +0100

    main menu commit

--- src/main/java/com/ironhack/wawgame/gameMenus/MainMenu.java
+++ src/main/java/com/ironhack/wawgame/gameMenus/MainMenu.java
@@ -1,5 +1,33 @@
 package com.ironhack.wawgame.gameMenus;
 
+import java.util.Scanner;
+
 public class MainMenu {
-    private
+    public static void main(String[] args){
+        Scanner sn = new Scanner(System.in);
+        boolean exit = false;
+        int option;
+
+        while(!exit){
+
+            System.out.println("Empezar juego");
+            System.out.println("Opciones");
+            System.out.println("Salir");
+
+            System.out.println("Escribe una de las opciones");
+            option = sn.nextInt();
+
+            switch(option){
+                case 1:
+                    System.out.println("Empieza el juego!");
+                    break;
+                case 2:
+                    System.out.println("Opciones");
+                    break;
+                case 3:
+                    exit=true;
+                    break;
+            }
+        }
+    }
 }
