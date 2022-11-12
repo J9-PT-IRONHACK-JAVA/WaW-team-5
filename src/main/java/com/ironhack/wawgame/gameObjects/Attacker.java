8bc0f7c5a0e5a3add178659e6950b38e0f4399d0
jose <jose245198@gmail.com>
2022-11-12 10:16:37 +0100

    comentarios Attacker

--- src/main/java/com/ironhack/wawgame/gameObjects/Attacker.java
+++ src/main/java/com/ironhack/wawgame/gameObjects/Attacker.java
@@ -1,11 +1,12 @@
 package com.ironhack.wawgame.gameObjects;
 
+//atacante
 public interface Attacker {
 
-
+//atacante hace un ataque
     public int doAttack();
-
+//recibe ataque con daño
     public void receiveAttack (int damage);
-
+//poder saber si está vivo o no
     public boolean getIsAlive ();
 }
